687e5f54bbee62c2fc149ec26ba6d9e6f4fb614a
Tunahan Kartal <kartalltunahan@gmail.com>
2025-08-13 10:03:20 +0300

    task tablosuna yeni foreign eklendi.

--- database/migrations/2025_08_12_111408_create_categories_table.php
+++ database/migrations/2025_08_12_111408_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_active')->default(1)->comment('0-inactive, 1-active');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
